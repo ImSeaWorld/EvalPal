66bc30492493c5a686e1558aa46385c72f9fda4e
SeaWorld <robert.klitsch97@gmail.com>
2022-08-03 20:23:37 -0700

    Added no-follow in meta and header for good measure

--- index.php
+++ index.php
@@ -4,12 +4,15 @@ $evalCheck = '$evalExists = true;';
 eval($evalCheck);
 
 $package = json_decode(file_get_contents(__DIR__ . '/package.json'));
+
+header('X-Robots-Tag: noindex, nofollow', true);
 ?>
 
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP EvalPal v<?php echo $package->version; ?></title>
     <meta name="description" content="<?php echo $package->description; ?>">
